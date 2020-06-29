4e6148d03484c77aa655398b615976dac93e3e09
Noah Sylwester <nanoabram@comcast.net>
2020-06-29 15:17:35 -0700

    finish group threading, invites, accepting and deleting invites, inviting by direct ID input, and add help messages from chatcat

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -7,38 +7,50 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string message)
+        public async Task SendMessage(string message, string groupName)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message, Context.ConnectionId);
-            // foreach (string userID in userIDs)
-            // {
-            //     await Clients.Client(userID).SendAsync("ReceiveMessage", user, message);
-            // }
+            if (groupName != "")
+            {
+                await Clients.Group(groupName).SendAsync("ReceiveMessage", message, Context.ConnectionId);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", message, Context.ConnectionId);
+            }
         }
         public async Task AddToGroup(string groupName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined the group '{groupName}'.", "chatcat");
         }
-
         public async Task RemoveFromGroup(string groupName)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
 
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} has left the group '{groupName}'.", "chatcat");
+        }
+        public async Task SendGroupInvite(string connectionID, string groupName)
+        {
+            await Clients.Client(connectionID).SendAsync("ReceiveInvite", Context.ConnectionId, groupName);
         }
         public override async Task OnConnectedAsync()
         {
             ConnectedUser.Ids.Add(Context.ConnectionId);
             await Clients.Caller.SendAsync("InitUser", Context.ConnectionId);
             await Clients.Caller.SendAsync("UsersOnline", ConnectedUser.Ids);
-            await base.OnConnectedAsync(); 
+            await Clients.Caller.SendAsync("ReceiveMessage", "Hi! Welcome to chatcat! To get started you can either create a room in the top right or join an existing one from invite!", "chatcat");
+            await Clients.Caller.SendAsync("ReceiveMessage", "Once you've created a room, you can invite others either by clicking their connection ID, or by entering it directly with 'invite by ID'", "chatcat");
+            await base.OnConnectedAsync();
         }
         public override async Task OnDisconnectedAsync(Exception exception) { 
             ConnectedUser.Ids.Remove(Context.ConnectionId);  
             await base.OnDisconnectedAsync(exception); 
          }
+        public async Task QueryUsersOnline()
+        {
+            await Clients.Caller.SendAsync("UsersOnline", ConnectedUser.Ids);
+        }
     }
     public static class ConnectedUser
     {
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,7 +1,9 @@
 "use strict";
-
+document.addEventListener("DOMContentLoaded", function(event) { 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-var connectionID = ''
+var connectionID = '';
+var groupName = '';
+var invites = [];
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
@@ -14,12 +16,88 @@ connection.on("InitUser", function (userID) {
 connection.on("UsersOnline", function (userIDs) {
     $("#usersOnlineCount").text(userIDs.length)
     const usersOnline = $("#usersOnline");
-    usersOnline.innerHTML = '';
+    $(usersOnline).html('')
     for (let i = 0; i < userIDs.length; i++) {
         const userElement = $("<li></li>");
         $(userElement).text(userIDs[i]).addClass("userIDListElement");
+        $(userElement).on("click", function(event) {
+            $("#inviteConnectionID").text(userIDs[i])
+            if (groupName === '') {
+                alert("create a room first!")
+            }
+            else {
+                $("#inviteRoomName").text(groupName)
+                $("#inviteUserModal").modal('show');
+            }
+        })
         $(usersOnline).append(userElement)
     }
+});
+
+function updateInvites() {
+    $('#invitesList').html('');
+    for (let i = 0; i < invites.length; i++) {
+        const invitedGroupName = invites[i][1];
+        const fromUserID = invites[i][0];
+        const row = $(`
+        <tr>
+            <td>${invitedGroupName}</td>
+            <td>${fromUserID}</td>
+            <td>
+                <button class="accept catButton">accept</button>
+            </td>
+            <td>
+                <button class="delete catButton">delete</button>
+            </td>
+        </tr>
+        `)
+        $(row).find(".accept").on("click", function(event) {
+            event.preventDefault();
+            connection.invoke("AddToGroup", invitedGroupName).catch(function (err) {
+                return console.error(err.toString());
+            });
+            invites.splice(invites.indexOf([fromUserID, invitedGroupName]),1);
+            updateInvites()
+            $("#seeInvitesModal").modal('hide')
+        })
+        $(row).find(".delete").on("click", function(event) {
+            invites.splice(invites.indexOf([fromUserID, invitedGroupName]),1);
+            updateInvites()
+        })
+        $('#invitesList').append(row)
+    }
+    $('#invitesCount').text(invites.length)
+}
+
+connection.on("ReceiveInvite", function (fromUserID, invitedGroupName) {
+    invites.push([fromUserID, invitedGroupName]);
+    const row = $(`
+    <tr>
+        <td>${invitedGroupName}</td>
+        <td>${fromUserID}</td>
+        <td>
+            <button class="accept catButton">accept</button>
+        </td>
+        <td>
+            <button class="delete catButton">delete</button>
+        </td>
+    </tr>
+    `)
+    $(row).find(".accept").on("click", function(event) {
+        event.preventDefault();
+        connection.invoke("AddToGroup", invitedGroupName).catch(function (err) {
+            return console.error(err.toString());
+        });
+        invites.splice(invites.indexOf([fromUserID, invitedGroupName]),1);
+        updateInvites()
+        $("#seeInvitesModal").modal('hide')
+    })
+    $(row).find(".delete").on("click", function(event) {
+        invites.splice(invites.indexOf([fromUserID, invitedGroupName]),1);
+        updateInvites()
+    })
+    $('#invitesList').append(row)
+    $('#invitesCount').text(invites.length)
 });
 
 connection.on("ReceiveMessage", function (message, fromUserID) {
@@ -51,7 +129,7 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     var message = document.getElementById("messageInput").value;
     if (message) {
         document.getElementById("messageInput").value = "";
-        connection.invoke("SendMessage", message).catch(function (err) {
+        connection.invoke("SendMessage", message, groupName).catch(function (err) {
             return console.error(err.toString());
         });
     }
@@ -65,12 +143,73 @@ document.addEventListener("keyup", function(event) {
         var message = document.getElementById("messageInput").value;
         if (message) {
             document.getElementById("messageInput").value = "";
-            connection.invoke("SendMessage", message).catch(function (err) {
+            connection.invoke("SendMessage", message, groupName).catch(function (err) {
                 return console.error(err.toString());
             });
         }
     }
 })
+
+// $("#inviteUserModal").on("show.bs.modal", function(event) {
+//     if (groupName === '') {
+//         console.log('here')
+//         $('#inviteUserModal').modal('toggle')
+//     }
+//     else {
+//         console.log('shown')
+//     }
+// })
+
+document.querySelector("#createRoomConfirmButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    const createdGroupName = $("#createGroupNameInput").val();
+    $("#createGroupNameInput").val('');
+    groupName = createdGroupName;
+    connection.invoke("AddToGroup", groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    $("#groupNameDisplay").text(groupName);
+    $('#createRoomModal').modal('hide');
+})
+
+document.querySelector("#inviteUserConfirmButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    const invitedConnectionID = $("#inviteConnectionID").text();
+    connection.invoke("SendGroupInvite", invitedConnectionID, groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    $('#inviteUserModal').modal('hide');
+})
+
+document.querySelector("#inviteDirectConfirmButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    const invitedConnectionID = $("#inviteDirectInput").val();
+    if (invitedConnectionID !== "") {
+        if (groupName === '') {
+            $("#errorDirectInvite").text("create a room first");
+        }
+        else {
+            connection.invoke("SendGroupInvite", invitedConnectionID, groupName).catch(function (err) {
+                return console.error(err.toString());
+            });
+            $("#errorDirectInvite").text('');
+            $("#inviteDirectInput").val('');
+            $('#inviteDirectModal').modal('hide');
+        }
+    }
+    else {
+        $("#errorDirectInvite").text("enter an ID");
+    }
+})
+
+document.querySelector("#refreshButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    connection.invoke("QueryUsersOnline").catch(function (err) {
+        return console.error(err.toString());
+    });
+})
+
 function handleConnectionIDChange(newConnectionID) {
     connectionID = newConnectionID;
 }
+});
