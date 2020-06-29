6102276884eab0cfb218df9f2594c5fe60507738
Noah Sylwester <nanoabram@comcast.net>
2020-06-29 01:07:30 -0700

    add basic chat function

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -7,18 +7,32 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message, string[] userIDs)
+        public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-            foreach (string userID in userIDs)
-            {
-                await Clients.Client(userID).SendAsync("ReceiveMessage", user, message);
-            }
+            await Clients.All.SendAsync("ReceiveMessage", message, Context.ConnectionId);
+            // foreach (string userID in userIDs)
+            // {
+            //     await Clients.Client(userID).SendAsync("ReceiveMessage", user, message);
+            // }
+        }
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
         }
         public override async Task OnConnectedAsync()
         {
             ConnectedUser.Ids.Add(Context.ConnectionId);
-            await Clients.Caller.SendAsync("InitUser", ConnectedUser.Ids);
+            await Clients.Caller.SendAsync("InitUser", Context.ConnectionId);
+            await Clients.Caller.SendAsync("UsersOnline", ConnectedUser.Ids);
             await base.OnConnectedAsync(); 
         }
         public override async Task OnDisconnectedAsync(Exception exception) { 
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,20 +1,43 @@
 "use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var connectionID = ''
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
-connection.on("InitUser", function (userIDs) {
-    console.log(userIDs);
+connection.on("InitUser", function (userID) {
+    connectionID = userID;
+    document.getElementById("connectionID").textContent = `Your connection ID: ${userID}`
 });
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("UsersOnline", function (userIDs) {
+    $("#usersOnlineCount").text(userIDs.length)
+    const usersOnline = $("#usersOnline");
+    usersOnline.innerHTML = '';
+    for (let i = 0; i < userIDs.length; i++) {
+        const userElement = $("<li></li>");
+        $(userElement).text(userIDs[i]).addClass("userIDListElement");
+        $(usersOnline).append(userElement)
+    }
+});
+
+connection.on("ReceiveMessage", function (message, fromUserID) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    var li = $("<li></li>");
+    var header = $("<p></p>");
+    var body = $("<p></p>");
+    $(header).addClass("chatMessageHeader");
+    $(body).addClass("chatMessageBody");
+    $(li).addClass('chatMessageItem');
+    if (fromUserID !== connectionID) {
+        $(li).addClass('remoteUserItem');
+    }
+    $(header).text(fromUserID);
+    $(body).text(msg);
+    $(li).append(header);
+    $(li).append(body);
+    $("#messagesList").append(li);
 });
 
 connection.start().then(function () {
@@ -24,10 +47,30 @@ connection.start().then(function () {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
+    // var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message, targetID).catch(function (err) {
-        return console.error(err.toString());
-    });
+    if (message) {
+        document.getElementById("messageInput").value = "";
+        connection.invoke("SendMessage", message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
     event.preventDefault();
 });
+
+document.addEventListener("keyup", function(event) {
+    const key = event.keyCode || event.which;
+
+    if (key === 13) {
+        var message = document.getElementById("messageInput").value;
+        if (message) {
+            document.getElementById("messageInput").value = "";
+            connection.invoke("SendMessage", message).catch(function (err) {
+                return console.error(err.toString());
+            });
+        }
+    }
+})
+function handleConnectionIDChange(newConnectionID) {
+    connectionID = newConnectionID;
+}
